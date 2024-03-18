904c417e6b400eae021b040050225276d490f2aa
Студент ПТК <student@ptk.ru>
2024-03-18 11:12:18 +0300

    2

---
+++ codecept.conf.ts
@@ -0,0 +1,24 @@
+import { setHeadlessWhen, setCommonPlugins } from '@codeceptjs/configure';
+// turn on headless mode when running with HEADLESS=true environment variable
+// export HEADLESS=true && npx codeceptjs run
+setHeadlessWhen(process.env.HEADLESS);
+
+// enable all common plugins https://github.com/codeceptjs/configure#setcommonplugins
+setCommonPlugins();
+
+export const config: CodeceptJS.MainConfig = {
+  tests: 'y',
+  output: './output',
+  helpers: {
+    Puppeteer: {
+      url: 'http://localhost',
+      show: true,
+      windowSize: '1200x900'
+    }
+  },
+  include: {
+    'Я': './steps_file'
+  },
+  name: 'ProjectTimofeev',
+  translation: 'ru-RU'
+}
